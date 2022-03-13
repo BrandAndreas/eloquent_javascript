5a87b4945aff7545d6c71fe9ebe19be28c18ea7d
BrandAndreas <brand.unter3@gmail.com>
2022-03-13 22:04:05 +0100

    Weitere Versuche an aList.js

--- 04_ArraysAndObjects/aList.js
+++ 04_ArraysAndObjects/aList.js
@@ -15,6 +15,32 @@ let list = {
     }
 };
 
+function arrayToList(...numbers) {
+    let newObj = {};
+    newObj = {value: numbers[numbers.length-1], rest: null};
+    
+    for(let i=numbers.length-2; i>=0; i--) {
+        newObj = {value: numbers[i], rest: newObj};
+    }
+    
+    return newObj;
+    
+}
+
+function listToArray(listObj) {
+    let newArray = [];
+    let newValue = Object.keys(listObj)[0];
+    console.log(newValue);
+    newArray.push(listObj.newValue);
+    console.log(newArray);
+}
+
+
+let numbers = [1,2,3];
+let newList = arrayToList(...numbers);
+console.log(newList)
+listToArray(newList);
+
 list = {value: 0, rest: list};
 list = {value: -1, rest: list};
 
