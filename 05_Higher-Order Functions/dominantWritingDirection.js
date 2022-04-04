0892b639a30a1a6da28773708b35ce84f7c325ad
BrandAndreas <brand.unter3@gmail.com>
2022-04-04 10:02:05 +0200

    finished exercices chapter 5

--- 05_Higher-Order Functions/dominantWritingDirection.js
+++ 05_Higher-Order Functions/dominantWritingDirection.js
@@ -1125,40 +1125,42 @@ var SCRIPTS = [
 
 
 function dominantDirection(text) {
-    function characterScript(code) {
-        for (let script of SCRIPTS) {
-          if (script.ranges.some(([from, to]) => {
-            return code >= from && code < to;
-          })) {
-            return script;
-          }
-        }
-        return null;
+  function characterScript(code) {
+    for (let script of SCRIPTS) {
+      if (script.ranges.some(([from, to]) => {
+        return code >= from && code < to;
+      })) {
+        return script;
       }
-
-    function countBy(items, groupName) {
-        let counts = [];
-        for (const item of items) {
-          let name = groupName(item);
-          let known = counts.findIndex(c => c.name == name);
-          if (known == -1) {
-            counts.push({name, count: 1});
-          } else {
-            counts[known].count++;
-          }
-        }
-        return counts;
     }
+    return null;
+  }
 
-    let scripts = countBy(text, char => {
-        let script = characterScript(char.codePointAt(0));
-        return script ? script.direction : "none";
-      }).filter(({name}) => name != "none");
+  function countBy(items, groupName) {
+    let counts = [];
+    for (const item of items) {
+      let direction = groupName(item);
+      let known = counts.findIndex(c => c.direction == direction);
+      if (known == -1) {
+        counts.push({direction, count: 1});
+      } else {
+        counts[known].count++;
+      }
+    }
+    return counts;
+  }
+
+  let scripts = countBy(text, char => {
+    let script = characterScript(char.codePointAt(0));
+    return script ? script.direction : "none";
+  }).filter(({direction}) => direction != "none");
+  
+  // scripts.direction[sc]
+  for (let i = 0; i < scripts.length; i++) {
+    const element = scripts[i];
     
-    return scripts;
-    
-    
-    // Your code here.
+  }
+  return scripts.reduce((a,b) => a.count > b.count ? a : b).direction;
 }
       
 console.log(dominantDirection("Hello!"));
