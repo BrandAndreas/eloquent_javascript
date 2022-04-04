52f770853ae26c22b0e8a4a12a1dbe1d8358fa53
BrandAndreas <brand.unter3@gmail.com>
2022-04-04 16:31:52 +0200

    Getters & Setters

--- testings/secretLivesOfObejcts.js
+++ testings/secretLivesOfObejcts.js
@@ -234,3 +234,39 @@ let matrix = new Matrix(3, 4, (x, y) => `value ${x},${y}`);
 for (let {x, y, value} of matrix) {
     console.log(x, y, value);
 }
+
+
+
+// --------------------------------------------------------
+// --------------Getters, Setters ans Statics--------------
+// --------------------------------------------------------
+
+let varyingSize = {
+    get size() {
+        return Math.floor(Math.random() * 100);
+    }
+};
+
+console.log(varyingSize.size);
+console.log(varyingSize.size);
+
+class Temperature {
+    constructor(celsius) {
+        this.celsius = celsius;
+    }
+    get fahrenheit() {
+        return this.celsius * 1.8 + 32;
+    }
+    set fahrenheit(value) {
+        this.celsius = (value -32) / 1.8;
+    }
+
+    static fromFahrenheit(value) {
+        return new Temperature((value - 32) / 1.8);
+    }
+}
+
+let temp = new Temperature(22);
+console.log(temp.fahrenheit);
+temp.fahrenheit = 86;
+console.log(temp.celsius);
