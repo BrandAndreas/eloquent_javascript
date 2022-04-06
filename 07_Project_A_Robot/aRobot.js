2626b2e67ac6872b578f21366234cd633caefecc
BrandAndreas <brand.unter3@gmail.com>
2022-04-06 15:51:46 +0200

    created infinity loop - argghh

--- 07_Project_A_Robot/aRobot.js
+++ 07_Project_A_Robot/aRobot.js
@@ -40,8 +40,8 @@ class VillageState {
         } else {
             let parcels = this.parcels.map(p => {
                 if (p.place != this.place) return p;
-                return {place: destination, adress: p.adress};
-            }).filter(p => p.place != p.adress);
+                return {place: destination, address: p.address};
+            }).filter(p => p.place != p.address);
             return new VillageState(destination, parcels);
         }
     }
@@ -49,7 +49,7 @@ class VillageState {
 
 let first = new VillageState(
     "Post Office",
-    [{place: "Post Office", adress: "Alice's House"}]
+    [{place: "Post Office", address: "Alice's House"}]
 );
 let next = first.move("Alice's House");
 
@@ -58,3 +58,39 @@ console.log(next.parcels);
 console.log('next: ', next);
 console.log(first.place);
 
+function runRobot(state, robot, memory) {
+    for (let turn = 0;; turn++) {
+        if (state.parcels.length == 0) {
+            console.log(`Done in ${turn} turns`);
+            break;
+        }
+        let action = robot(state, memory);
+        state = state.move(action.direction);
+        memory = action.memory;
+        console.log(`Moved to ${action.direction}`);
+    }
+}
+
+function randomPick(array) {
+    let choice = Math.floor(Math.random() * array.length);
+    return array[choice];
+}
+
+function randomRobot(state) {
+    return {direction: randomPick(roadGraph[state.place])};
+}
+
+VillageState.random = function(parcelCount = 5) {
+    let parcels = [];
+    for (let i = 0; i < parcelCount; i++) {
+        let address = randomPick(Object.keys(roadGraph));
+        let place;
+        do {
+            place = randomPick(Object.keys(roadGraph));
+        } while (place == address);
+        parcels.push({place, address});
+    }
+    return new VillageState('Post Office', parcels);
+};
+
+runRobot(VillageState.random(), randomRobot);
