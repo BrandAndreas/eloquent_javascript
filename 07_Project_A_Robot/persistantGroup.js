6d383730dd70f2304f3f78e7011badf8a1e9705c
BrandAndreas <brand.unter3@gmail.com>
2022-04-11 15:32:03 +0200

    starting persistant Group

--- 07_Project_A_Robot/persistantGroup.js
+++ 07_Project_A_Robot/persistantGroup.js
@@ -1,14 +1,44 @@
 class PGroup {
-    // Your code here
-  }
-  
-  let a = PGroup.empty.add("a");
-  let ab = a.add("b");
-  let b = ab.delete("a");
-  
+    constructor() {
+        this.members = [];
+        this.empty = new PGroup;
+    }
+    
+      add(value) {
+        if (!this.has(value)) {
+          this.empty.members.push(value);
+        }
+    }
+    
+      delete(value) {
+        this.members = this.members.filter(v => v !== value);
+    }
+    
+      has(value) {
+        return this.members.includes(value);
+    }
+    
+    
+
+    static from(collection) {
+        let group = new PGroup;
+        for (let value of collection) {
+          group.add(value);
+        }
+        return group;
+      }
+}
+
+// let a = PGroup.empty;
+// console.log(a);
+let a = PGroup.empty.add("a");
+let ab = a.add("b");
+let b = ab.delete("a");
+/* let a = PGroup.empty.add("a");
+
   console.log(b.has("b"));
   // → true
   console.log(a.has("b"));
   // → false
   console.log(b.has("a"));
-  // → false
+  // → false */
