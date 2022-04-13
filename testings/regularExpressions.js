78f058e0fc0f67e4a6718be6245a315624032dca
BrandAndreas <brand.unter3@gmail.com>
2022-04-13 16:29:44 +0200

    functions in regex

--- testings/regularExpressions.js
+++ testings/regularExpressions.js
@@ -17,8 +17,9 @@ console.log(/\d{1,2}/.exec("100"));
 console.log(/(\d{1,2})/.exec("100")); // [ "10", "10" ]
 console.log(/(\d{1,2})-(\d{1,2})/.exec("100-10")); // [ "00-10", "00", "10" ]
 
-console.log(/\bm/.exec("moon")); // [m]
+console.log(/\bm/.exec("moon")); // [ "m" ]
 console.log(/\bm/.exec("1moon")); // null
+console.log(/\bm/.exec("1 moon")); // [ "m" ]
 console.log(/on\b/.exec("moon")); // [ "on" ]
 console.log(/oo\b/.exec("moon")); // null
 
@@ -38,4 +39,41 @@ console.log(
 John McCarthy
 Philip Wadler */
 
-let s = 
+let s = "the cia and fbi";
+console.log(s.replace(/\b(fbi|cia)\b/g,
+    str => str.toUpperCase())); // the CIA and FBI
+
+let stock = "1 lemon, 2 cabbages, and 101 eggs";
+function minusOne(match, amount, unit) {
+    console.log(match);
+    amount = Number(amount) - 1;
+    if (amount == 1) { //only one left, remove the 's'
+        uni = unit.slice(0, unit.length - 1);
+    } else if (amount == 0) {
+        amount = "no";
+    }
+    return amount + " " + unit;
+}
+console.log(stock.replace(/(\d+) (\w+)/g, minusOne)); // no lemon, 1 cabbages, and 100 eggs
+
+
+function stripComments(code) {
+    return code.replace(/\/\/.*|\/\*[^]*?\*\//g, "");
+}
+
+console.log(stripComments("1 + /* 2 */3")); // 1 + 3
+console.log(stripComments("x = 10;// ten!")); // x = 10;
+console.log(stripComments("1 /* a */+/* b */ 1")); // 1 + 1
+
+
+let name = "harry";
+let text = "Harry is a suspicious character.";
+let regexp = new RegExp("\\b(" + name + ")\\b", "gi");
+console.log(text.replace(regexp, "_$1_")); // _Harry_ is a suspicious character.
+
+let neardyName = "dea+hl[]rd";
+let neardyText = "This dea+hl[]rd guy is super annoying";
+let escaped = neardyName.replace(/[\\[.+*?(){|^$]/g, "\\$&");
+console.log(escaped);
+let neardyRegexp = new RegExp("\\b" + escaped + "\\b", "gi");
+console.log(neardyText.replace(neardyRegexp, "_$&_"));
