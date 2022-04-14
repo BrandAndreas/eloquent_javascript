e46bdebf4729e33bdf951954a0a9feb46d682932
BrandAndreas <brand.unter3@gmail.com>
2022-04-14 13:44:34 +0200

    match regex

--- testings/regularExpressions.js
+++ testings/regularExpressions.js
@@ -82,11 +82,11 @@ console.log(neardyText.replace(neardyRegexp, "_$&_"));
 console.log("   word".search(/\S/)); // 3
 console.log("    ".search(/\S/)); // -1
 
-let pattern = /y/g;
+/* let pattern = /y/g;
 pattern.lastIndex = 3;
 let match = pattern.exec("xyzzy");
 console.log(match.index); // 4
-console.log(pattern.lastIndex); // 5
+console.log(pattern.lastIndex); // 5 */
 
 let global = /abc/g;
 console.log(global.exec("xyz abc")); // [ "abc" ]
@@ -97,3 +97,39 @@ console.log(sticky.exec("xyz abc")); // null
 let digit = /\d/g;
 console.log(digit.exec("here it ist: 1")); // [ "1" ]
 console.log(digit.exec("and now: 1")); // null
+
+console.log("Banana".match(/an/g)); // [ "an", "an" ]
+console.log("Banana".match(/an/)); // [ "an" ]
+
+let input = "A string with 3 numbers in it... 42 and 88.";
+let number = /\b\d+\b/g;
+let newMatch;
+while (newMatch = number.exec(input)) {
+    console.log("Found", newMatch[0], "at", newMatch.index);
+}
+/* Found 3 at 14
+Found 42 at 33
+Found 88 at 40 */
+
+
+function parseINI(string) {
+    // Start with an object to hold the top-level fields
+    let result = {};
+    let section = result;
+    string.split(/\r?\n/).forEach(line => {
+        let match;
+        if (match = line.match(/^(\w+)=(.*)$/)) {
+            section[match[1]] = match[2];
+        } else if (match = line.match(/^\[(.*)\]$/)) {
+            section = result[match[1]] = {};
+        } else if (!/^\s*(;.*)?$/.test(line)) {
+            throw new Error("Line '" + line + "' is not valid.");
+        }
+    });
+    return result;
+}
+
+console.log(parseINI(`
+name=Vasilis
+[adress]
+city=Tessaloniki`)); // Object { name: "Vasilis", adress: {city: "Tessaloniki"} }
