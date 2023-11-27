51b9c2edb5ad866d3523d76bbcb9d810b748d2a1
BrandAndreas <brand.unter3@gmail.com>
2023-11-27 14:31:12 +0100

    iterator finished

--- 06_Objekte/02_Gruppe.js
+++ 06_Objekte/02_Gruppe.js
@@ -29,26 +29,48 @@ class Group {
         return this.values.some(val => val === value)
     }
     static from(iteratableObject) {
-        console.log(iteratableObject.length);
+        const newGroup = new Group();
         for(const element of iteratableObject) {
-            if(element == null || isNaN(element)) {
-                continue;
-            }
-            if(this.values.indexOf(element) === -1) {
-                this.values.push(element);
-            }
+            newGroup.add(element);
         }
-        return this.values;
+        return newGroup.values;
     }
 }
 
-const newGroup = new Group();
-console.log(newGroup.add(2));
-console.log(newGroup.add(4));
-console.log(newGroup.add(6));
-console.log(newGroup.add());
-console.log(newGroup.delete(4));
-console.log(newGroup.values);
-console.log(newGroup.has(6));
-console.log(newGroup.has(4));
-console.log(Group.from([1,3,5]));
+class GroupIterator {
+    constructor(group) {
+        this.counter = 0;
+        this.group = group;
+    }
+
+    next() {
+        if(this.counter = this.group.values.length) {
+            return {done: true};
+        }
+
+        let value = this.group.values[this.counter];
+        counter++;
+        return  {
+            value, done:false
+        };
+    }
+}
+
+Group.prototype[Symbol.iterator] = function() {
+    return new GroupIterator(this);
+}
+
+// const newGroup = new Group();
+// console.log(newGroup.add(2));
+// console.log(newGroup.add(4));
+// console.log(newGroup.add(6));
+// console.log(newGroup.add());
+// console.log(newGroup.delete(4));
+// console.log(newGroup.values);
+// console.log(newGroup.has(6));
+// console.log(newGroup.has(4));
+// console.log(Group.from([1,3,3, ,5]));
+const wonderfulGroup = Group.from([1,3,5]);
+for(const value of wonderfulGroup)  {
+    console.log(value);
+}
