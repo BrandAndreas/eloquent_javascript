eef488cdfbb61f789d14f81a4e5ad65d22b85d5e
BrandAndreas <brand.unter3@gmail.com>
2023-12-07 22:00:50 +0100

    best Robot started

---
+++ 07_Roboter/bestRobot.js
@@ -0,0 +1,28 @@
+function findRoute(graph, from, to) {
+    let work = [{at: from, route: []}];
+    for (let i = 0; i < work.length; i++) {
+        let {at, route} = work[i];
+        for (let place of graph[at]) {
+            if (place == to) return route.concat(place);
+            if (!work.some(w => w.at == place)) {
+                work.push({at: place, route: route.concat(place)});
+            }
+        }
+    }
+}
+
+function goalOrientedRobot({place, parcels}, route) {
+    if (!route || route.length == 0) {
+        let parcel = parcels[0]; // Das parcel ist das erste Paket der Pakete
+        if (parcel.place != place) { // Wenn das Pakte noch nicht angekommen ist
+            route = findRoute(roadGraph, place, parcel.place);
+        } else {
+            route = findRoute(roadGraph, place, parcel.address);
+        }
+    }
+    return {direction: route[0], memory: route.slice(1)};
+}
+
+function bestRobot() {
+
+}
