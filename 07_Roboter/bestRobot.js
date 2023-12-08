5995633cafe96b5c98dd72a8e393d44016f54b9e
BrandAndreas <brand.unter3@gmail.com>
2023-12-08 15:19:39 +0100

    f

--- 07_Roboter/bestRobot.js
+++ 07_Roboter/bestRobot.js
@@ -1,12 +1,15 @@
+// PARCELS:
+// {"place":"Post Office","parcels":[{"place":"Ernie's House","address":"Shop"},{"place":"Alice's House","address":"Town Hall"},{"place":"Post Office","address":"Bob's House"},{"place":"Cabin","address":"Town Hall"},{"place":"Alice's House","address":"Marketplace"}]}
+
 function findRoute(graph, from, to) {
     let work = [{at: from, route: []}];
     for (let i = 0; i < work.length; i++) {
         let {at, route} = work[i];
-        for (let place of graph[at]) {
-            if (place == to) return route.concat(place);
+        for (let place of graph[at]) { // Für alle direkten Verbindungen zum aktuellen Ort
+            if (place == to) return route.concat(place); // Wenn es eine direkte Verbindung zum Ziel gibt, packe die Verbindung in die Route und gib die Route zurück
             if (!work.some(w => w.at == place)) {
                 work.push({at: place, route: route.concat(place)});
-            }
+            } // Wenn es keine Verbindung gibt, dann gib dem Arbeitsarray den neuen Platz hinzu und der Route auch
         }
     }
 }
@@ -22,6 +25,13 @@ function goalOrientedRobot({place, parcels}, route) {
     }
     return {direction: route[0], memory: route.slice(1)};
 }
+
+/* 
+IDEE:
+Schau dir erst alle places der Pakete an. Geh zu dem, dass am nächsten ist. Kiche dieses Ziel aus der Liste.
+Dann packe das Ziel dieses Paketes in die Liste und vergleiche, ob diese Adresse oder die places der übrigen Pakete am nächsten ist.
+Gehe dann zum nächsten Ziel und fahre weiter so fort.
+*/
 
 function bestRobot() {
 
