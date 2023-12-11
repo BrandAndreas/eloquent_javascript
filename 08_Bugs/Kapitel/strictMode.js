109420d170b798b6b12a8762c840c4a7cdf49f59
BrandAndreas <brand.unter3@gmail.com>
2023-12-11 11:49:53 +0100

    started bugs chapter

---
+++ 08_Bugs/Kapitel/strictMode.js
@@ -0,0 +1,21 @@
+/* function canYouSpotTheProblem() {
+    "use strict";
+    for (counter = 0; counter < 10; counter++) {
+        console.log('Happy happy');
+    }
+}
+
+canYouSpotTheProblem(); 
+
+function Person(name) { this.name = name; }
+let ferdinand = Person("Ferdinand");
+console.log(name);
+
+
+"use strict";
+function Person(name) { this.name = name; }
+let ferdinand = new Person("Ferdinand");
+console.log(ferdinand.name);
+
+*/
+
