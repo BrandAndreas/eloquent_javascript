8ec3f81e1d42377b76f6b22f7bf9a0156b76aa78
BrandAndreas <brand.unter3@gmail.com>
2023-12-11 10:29:34 +0100

    pGroup finished

--- 07_Roboter/pGroup.js
+++ 07_Roboter/pGroup.js
@@ -12,7 +12,7 @@ class PGroup {
         }
         if(this.values.indexOf(value) === -1) {
             this.empty = new PGroup();
-
+            this.values.forEach(val => this.empty.values.push(val));
             this.empty.values.push(value);
             return this.empty;
         }
@@ -23,20 +23,26 @@ class PGroup {
         }
         if(this.values.indexOf(value) !== -1) {
             this.empty = new PGroup();
-            this.empty.values = this.values;
-            console.log(`this.values = ${this.values}`);
-            console.log(this.empty.values);
-
+            this.values.forEach(val => this.empty.values.push(val));
             this.empty.values.splice(this.empty.values.indexOf(value), 1);
-            console.log(`this.values = ${this.values}`);
             return this.empty;
         }
+    }
+    has(value) {
+        if(value == null || isNaN(value)) {
+            return;
+        }
+        return this.values.some(val => val === value)
     }
 }
 
 let firstGroup = new PGroup();
 let secondGroup = firstGroup.add('2');
 let thirdGroup = secondGroup.add('3');
+let fourthGroup = thirdGroup.delete('2');
 console.log(firstGroup.values);
 console.log(secondGroup.values);
 console.log(thirdGroup.values);
+console.log(fourthGroup.values);
+console.log(thirdGroup.values);
+console.log(thirdGroup.has('3'));
